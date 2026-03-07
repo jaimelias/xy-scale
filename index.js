60f6a3ed34d06b7f410f93490ace684a53f17ce2
Jaime Castillo <escribeme@jaimelias.com>
2026-03-06 22:49:39 -0500

    added arrayShuffle, arrayShuffleXY to export

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { parseTrainingXY, parseProductionX } from "./src/datasets.js"
 import {arrayToTimesteps } from "./src/timeSteps.js"
 import { arrayShuffle, arrayShuffleXY } from "./src/utilities.js"
 
-export { parseTrainingXY, parseProductionX, arrayToTimesteps }
+export { parseTrainingXY, parseProductionX, arrayToTimesteps, arrayShuffle, arrayShuffleXY }
